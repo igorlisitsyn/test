044dd0ac21509f29ec55d513c9470b4482c7a175
Igor Lisitsyn <bit1@list.ru>
2018-06-04 15:19:10 +1200

    чтение и запись в csv файл

--- real_csv.py
+++ real_csv.py
@@ -5,6 +5,16 @@ def write_csv(data):
         writer = csv.writer(f)
         writer.writerow((data['name'], data['surname'], data['age']))
 
+
+def write_csv2(data):
+    with open('name2.csv', 'a') as f:
+        order = ['name', 'surname', 'age']
+        writer = csv.DictWriter(f, fieldnames=order)
+        writer.writerow(data)
+
+
+
+
 def main():
     d = {'name': 'Petr', 'surname': 'Ivanov', 'age': 20}
     d1 = {'name': 'Oksana', 'surname': 'Petrova', 'age': 18}
@@ -12,9 +22,14 @@ def main():
 
     l = [d, d1, d2]
 
-    for i in l:
-        write_csv(i)
+#    for i in l:
+#        write_csv(i)
+    with open('cmc.csv', 'r') as f:
+        fil = ['name', 'sokr', 'url', 'dev']
+        reader = csv.DictReader(f, fieldnames=fil)
 
+        for row in reader:
+            print(row)
 
 
 
